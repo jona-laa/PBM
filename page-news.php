07766ee60508bd84e27e9304cee428b8ef9186ce
Jonathan Laasonen <iphntm@iPHNTMs-MBP.lan>
2020-10-04 22:56:39 +0200

    add -news article | news page | news section on front page

--- page-news.php
+++ page-news.php
@@ -1,29 +1,44 @@
 <?php
+get_header();
+?>
+<div class="container">
+<section class="news">
+<h1 class="section-header"><?php the_title();?></h1>
+
+<?php
+    $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
     $args = array(
         'post_type' => 'post',
         'post_status' => 'publish',
-        'category_name' => 'News Flash',
-        'posts_per_page' => 1,
+        'category_name' => 'News',
+        'posts_per_page' => 4,
+        'paged' => $paged
     );
     $arr_posts = new WP_Query( $args );
     if ($arr_posts->have_posts()) :
         while ($arr_posts->have_posts()) :
             $arr_posts->the_post(); ?>
-            <section class="news">
-                <h2 class="section-header">News</h2>
                 <article class="news-article">
                     <a href="<?php the_permalink(); ?>" class="article-heading"><?php the_title('<h2 class="article-heading">', '</h2>'); ?></a>
-                    <?php
+                    <?php 
                         the_excerpt(); 
                     ?>
                     <div class="pub-info">
-                        <span class="pub-date"><?php the_modified_date(); ?></span>
+                        <span class="pub-date"><?php the_modified_date(); ?></span> 
                         <span class="auth-name"><?php the_author(); ?></span>
                     </div>
                 </article>
                 <div class="section-divider"></div>
-            </section>
-        <?php endwhile;
-        wp_reset_postdata();
+                <?php endwhile;
+        wp_pagenavi(
+            array(
+            'query' => $arr_posts,
+            )
+            );
     endif;
+?>
+</section>
+</div>
+<?php
+get_footer();
 ?>
---
+++ single.php
@@ -0,0 +1,31 @@
+<?php get_header() ?>
+<?php the_post() ?>
+<div class="container">
+  <section class="news">
+    <article class="news-article">
+      <h1 class="section-header"><?php the_title();?></h1>
+        <figure>
+            <?php
+                // If there is a thumbnail
+                if (has_post_thumbnail()){
+                    the_post_thumbnail('medium');
+                }
+            ?>
+            <figcaption>
+                <?php    
+                    // If there is a caption
+                    if (the_post_thumbnail_caption()) { 
+                        the_post_thumbnail_caption(); 
+                        }
+                ?>
+            </figcaption>
+        </figure>
+        <?php the_content(); ?>
+        <div class="pub-info">
+          <span class="pub-date"><?php the_modified_date(); ?></span> <span
+              class="auth-name"><?php the_author(); ?></span>
+        </div>
+    </article>
+  </section> 
+</div>
+<?php get_footer() ?>
